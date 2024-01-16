21ca8588b62c44dc141ed27bc765ae54c96072ae
Ronald De Mesa <ronaldreyjr061201@gmail.com>
2024-01-16 08:02:41 +0800

    Added navigate function

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,21 @@ import {
 import "./App.css";
 import RootLayout from "./layouts/RootLayout";
 import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<RootLayout />}>
         <Route index element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard/>}/>
       </Route>
     )
   );
+  /* 
+  if you will use loader data,
+  create the function at the compoonent as another variable outside of the function,
+  then use the useLoaderData() function to invoke it */
   return (
     <div className="h-screen w-screen">
       <RouterProvider router={router} />
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import "../App.css";
 import axios from "axios";
-import { redirect } from "react-router-dom";
+import { redirect, useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate();
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const usernameHandler = (e) => {
@@ -12,27 +13,26 @@ function Login() {
   const passwordHandler = (e) => {
     setPassword(e.target.value);
   };
-  const authenticateDetails = (e) => {
+  const authenticateDetails = async (e) => {
     e.preventDefault();
-    if(username.length === 0 || password.length === 0){
+    if (username.length === 0 || password.length === 0) {
       //do something to show validation
-      if(username.length === 0){
+      if (username.length === 0) {
         //show that username needs to have an input
-      }else if(password.length === 0){
+      } else if (password.length === 0) {
         //show that password needs to have an input
-      }else{
+      } else {
         //show that both needs to have an input
       }
-    }else if(username.length !== 0 && password.length !== 0){
+    } else if (username.length !== 0 && password.length !== 0) {
       //validate email here
-       const userAuthentication = (async () => {
-        const response = await axios.get('https://qk-app-backend.vercel.app/record/count');
-        const data = response.data;
-        if(data.access_email === username && data.access_password === password){
-          redirect(`/dashboard`);
-          return data;
-        }
-      })();
+      const response = await axios.get(
+        "https://qk-app-backend.vercel.app/record/count"
+      );
+      const data = await response.data[0];
+      if (data.access_email === username && data.access_password === password) {
+      }
+      return navigate("/dashboard");
       /* 
       
       MAKE SURE THE VALIDATION OR AUTHENTICATION IS CORRECT HERE
@@ -40,14 +40,15 @@ function Login() {
       
       */
     }
-    console.log("username", username);
-    console.log("password", password);
-  }
-  
+  };
+
   return (
     <div className="w-full mt-20 flex flex-col items-center justify-center ">
       <div className="px-20 py-14 bg-blue-400 flex flex-1 flex-col justify-center items-center rounded-3xl">
-        <form onSubmit={authenticateDetails} className="flex flex-col justify-around items-center">
+        <form
+          onSubmit={authenticateDetails}
+          className="flex flex-col justify-around items-center"
+        >
           <label htmlFor="username">Username:</label>
           <br />
           <input
@@ -61,8 +62,13 @@ function Login() {
           <br />
           <label htmlFor="password">Password:</label>
           <br />
-          <input type="password" name="password" id="password" onChange={passwordHandler}
-            value={password}/>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={passwordHandler}
+            value={password}
+          />
           <br />
           <button
             className="w-full py-3 rounded-full bg-white text-black border border-purple-500 hover:opacity-80"
